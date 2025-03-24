63edea0c9b947994bdc3de212538cf1b4f260dbb
Juan Manuel Bautista <104174520+Bauti567@users.noreply.github.com>
2025-03-24 16:49:16 -0500

    Delete backend directory

--- backend/src/config.js
+++
@@ -1,7 +0,0 @@
-export const DB_USER = "postgres"
-export const DB_HOST = "localhost"
-export const DB_PASSWORD = "12345"
-export const DB_DATABASE = "Prueba"
-export const DB_PORT = 5432
-
-export const PORT = process.env.PORT || 3000;
